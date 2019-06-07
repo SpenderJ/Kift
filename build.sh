52d7ac336cfd017181dcdcb541dc6c9dcb320212
Jules Spender <pro.jspender@gmail.com>
2019-06-07 13:27:33 -0700

    Depedencies

--- build.sh
+++ build.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
+brew install sox
 brew tap watsonbox/cmu-sphinx
 brew install --HEAD watsonbox/cmu-sphinx/cmu-sphinxbase
 brew install --HEAD watsonbox/cmu-sphinx/cmu-pocketsphinx
 cd client
+npm install
 npm run build
 rm -irf ../site/*
 cp -R build/* ../site/
